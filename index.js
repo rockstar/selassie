5ca90395647670340dcc4553ced1d1cb271fddbe
Paul Hummer <paul@eventuallyanyway.com>
2011-08-21 16:45:38 -0600

    Add get, use /get/test to test

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var PORT = 8000;
 var express = require('express');
 var app = express.createServer();
+var storage = {test: 'success!'};
+
+app.get('/get/:key', function(req, res) {
+    res.send(storage[req.params.key]);
+});
 
 console.log('Serving on port ' + PORT);
 app.listen(PORT);
