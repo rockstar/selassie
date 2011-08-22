972860e71bbee4a912bcd91387477a2e0c5d5603
Paul Hummer <paul@eventuallyanyway.com>
2011-08-21 18:42:04 -0600

    Added /set support

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var PORT = 8000;
 var express = require('express');
 var app = express.createServer();
-var storage = {test: 'success!'};
+var storage = {};
 
 app.get('/get/:key', function(req, res) {
     var val = storage[req.params.key];
@@ -9,6 +9,14 @@ app.get('/get/:key', function(req, res) {
     val = '"' + val + '"';
     if (req.query.callback) {
         val = req.query.callback + '(' + val + ')';
+    }
+    res.send(val);
+});
+app.get('/set/:key/:val', function(req, res) {
+    var val = '';
+    storage[req.params.key] = req.params.val;
+    if (req.query.callback) {
+        val = req.query.callback + '()';
     }
     res.send(val);
 });
